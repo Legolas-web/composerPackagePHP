df883bfcc33fb7574b420e49a5c0a01ee23a969d
Juan Borda <jeborda3@misena.edu.co>
2021-06-20 11:16:37 -0500

    primer commit

---
+++ example/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    include __DIR__ . "/../vendor/autoload.php";
+
+    $hola = new \Juan1\hola\Hola();
+
+    $hola -> diHola('Juan');
+
+
+?>
---
+++ src/Hola.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Juan1\hola;
+
+    class Hola{
+
+        public function diHola($message){
+            echo "Hola $message como estas?\n";
+        }
+
+    }
+
+?>
